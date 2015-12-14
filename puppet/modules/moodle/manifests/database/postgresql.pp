8062087b61dedb5ed8e15f8404592efd6628a581
ryanwyllie <ryanwyllie88@gmail.com>
2015-12-14 13:30:03 +0800

    Added php7 packges (commented out) to install that if you want

--- puppet/modules/moodle/manifests/database/postgresql.pp
+++ puppet/modules/moodle/manifests/database/postgresql.pp
@@ -1,6 +1,9 @@
 class moodle::database::postgresql (
 	$postgres_password = 'postgres',
 ) {
+
+	class { 'postgresql::client': }
+
 	class { 'postgresql::server':
         postgres_password => $postgres_password,
 		require => Exec['apt-update']
--- puppet/modules/moodle/manifests/packages.pp
+++ puppet/modules/moodle/manifests/packages.pp
@@ -1,11 +1,16 @@
 class moodle::packages (
 	$package_ensure = $moodle::params::package_ensure
 ) inherits moodle::params {
+
+	/*	
+	exec { "add php7 repo" :
+        command => 'add-apt-repository ppa:ondrej/php-7.0',
+    } -> */
 	exec { "apt-update" :
         command => '/usr/bin/apt-get -y update',
     }
 
-    $packages = [
+	$packages = [
         'php5',
         'php5-pspell',
         'php5-curl',
@@ -14,11 +19,26 @@ class moodle::packages (
         'php5-xmlrpc',
         'php5-ldap',
         'php5-pgsql',
+		'ghostscript',
+    ]
+
+	/*
+    $packages = [
+        'php7.0',
+        'php7.0-pspell',
+        'php7.0-curl',
+        'php7.0-gd',
+        'php7.0-intl',
+        #'php7.0-xmlrpc',
+        'php7.0-ldap',
+        'php7.0-pgsql',
         'ghostscript',
     ]
+	*/
 
     package { $packages:
         ensure => $package_ensure,
+        #require => Exec['add php7 repo', 'apt-update']
         require => Exec['apt-update']
     }
 }
